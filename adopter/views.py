1e6bd1d3925ab7d9ed0c8701c874936cd5e3aeec
Topaz Aakal <topazaakal5@gmail.com>
2021-12-06 16:10:35 +0200

    fixing the menu

--- adopter/views.py
+++ adopter/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render, get_object_or_404, redirect
-
-from django.http import HttpResponseRedirect
-
+from django.shortcuts import render
 from Animal.models import animal
 
 
 def home(request):
-    animals = animal.objects.all()
-    return render(request, 'adopter/home.html', {'animals': animals})
+     #animals = animal.objects.all()
+     return render(request, 'adopter/home.html')
 
