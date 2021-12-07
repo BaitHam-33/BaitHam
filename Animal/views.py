2d5d1942fc92b0f0285a6c1ba61078c874efe300
AfikDanan <Afikdanan24@gmail.com>
2021-12-07 22:32:48 +0200

    fixed image upload bug

--- Animal/views.py
+++ Animal/views.py
@@ -14,21 +14,36 @@ def add_Animal(request):
     if request.method == 'GET':
         return render(request, 'Animal/add_Animal.html', {'form': Add_Animal_Form()})
     else:
-        form = Add_Animal_Form(request.POST or None)
-        context = { "form": form }
+        form = Add_Animal_Form(request.POST, request.FILES)
         if form.is_valid():
-            context['form'] = Add_Animal_Form()
-            new_animal = form.save()
-            new_animal.save()
+            name = form.cleaned_data.get("name")
+            submitter= form.cleaned_data.get("submitter")
+            species = form.cleaned_data.get("species")
+            breed = form.cleaned_data.get("breed")
+            description = form.cleaned_data.get("description")
+            sex = form.cleaned_data.get("sex")
+            submission_date = form.cleaned_data.get("submission_date")
+            image = form.cleaned_data.get("image")
+            obj = animal.objects.create(
+                name=name,
+                submitter=submitter,
+                species=species,
+                breed=breed,
+                description=description,
+                sex=sex,
+                submission_date=submission_date,
+                image=image
+            )
+            obj.save()
         else:
-            print(form.errors) # incase of errors in validation
+            print(form.errors) # in case of errors in validation
         return redirect('Animal:all_animals')
+
 
 def Animal_detail(request, id=None):
     animal_obj = None
     if id is not None:
         animal_obj = animal.objects.get(id=id)
-    context = {
-        "object": animal_obj
-    }
+    context = {"object": animal_obj}
     return render(request, 'Animal/animal_detail.html', context=context)
+
