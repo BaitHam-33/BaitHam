60e0773276a8a930a1d09d349d2cae21272cca7c
Topaz Aakal <topazaakal5@gmail.com>
2021-12-14 11:13:05 +0200

    add comments to event app - topaz

--- Event/views.py
+++ Event/views.py
@@ -2,22 +2,27 @@ from django.shortcuts import render, redirect
 from .models import event
 from .forms import EventForm
 
+
 def createEvent(request):
+    """the function creat a new event according to the request of the user(admin only) and save it in the database"""
     if request.method == 'GET':
         return render(request, 'Event/createEvent.html', {'form': EventForm()})
     else:
-        form = EventForm(request.POST)
+        form = EventForm(request.POST)  # creat a form for the event
         new_event = form.save(commit=False)
-        new_event.save()
-        return redirect('Event:all_events')
+        new_event.save()  # saving the new event in the database
+        return redirect('Event:all_events')  # refers to the page of all events
 
 
 def all_events(request):
+    """""the function presents all events"""
     events = event.objects.order_by('-date')
     return render(request, 'Event/all_events.html', {'events': events})
 
 
 def deleteEvent(request, event_id):
+    """the function delete a event according to the request of the user (admin only) and delete it
+         from the database """
     Event = event.objects.get(pk=event_id)
-    Event.delete()
-    return redirect('Event:all_events')
+    Event.delete() # delete the report from the database
+    return redirect('Event:all_events') # refers to the page of all events
