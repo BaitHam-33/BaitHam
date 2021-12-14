532f998e36f17b533a1a35c5891496e7556c4e24
Topaz Aakal <topazaakal5@gmail.com>
2021-12-14 11:25:00 +0200

    add comments for volunteer app

--- volunteer/views.py
+++ volunteer/views.py
@@ -3,25 +3,27 @@ from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm
 
-"""
-def homeVolunteer(request):
-    return render(request,'volunteer/homeVolunteer.html')"""
 
 def logoutuser(request):
+    """function for disconnecting the user from the system"""
     if request.method == 'POST':
         logout(request)
     return redirect('volunteer:loginuser')
 
+
 def loginuser(request):
+    """function for connecting the user to the system"""
     if request.method == 'POST':
-        form = AuthenticationForm(request,data=request.POST)
-        if form.is_valid():
-            user=form.get_user()
-            login(request, user)
-            return redirect('home')
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():  # check if the details is valid
+            user = form.get_user()
+            login(request, user)  # connect the user to the system
+            return redirect('home')  # refer to the homepage
         else:
-            return render(request,'volunteer/loginuser.html',{'form':AuthenticationForm(),'error':'username and password did not match'})
+            # if the username or password are invalid, a message will appear accordingly
+            return render(request, 'volunteer/loginuser.html',
+                          {'form': AuthenticationForm(), 'error': 'username and password did not match'})
     else:
         form = AuthenticationForm(request)
         context = {'form': form}
-    return render(request, 'volunteer/loginuser.html',context)
+    return render(request, 'volunteer/loginuser.html', context)
