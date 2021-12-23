e5935d09eb67ec99d9819f9fb1a98eaa5118a35d
AfikDanan <Afikdanan24@gmail.com>
2021-12-23 22:40:31 +0200

    hebrew ad animal form

--- Animal/forms.py
+++ Animal/forms.py
@@ -8,3 +8,4 @@ class Add_Animal_Form(ModelForm):
     class Meta:
         model = animal
         fields = ['name', 'submitter', 'species', 'breed', 'description', 'sex', 'Adoption', 'submission_date', 'image']
+
--- Animal/models.py
+++ Animal/models.py
@@ -6,16 +6,16 @@ class animal(models.Model):
     Sex_Choice = [('M', 'Male'), ('F', 'Female')]
     Species_choice = [('dog', 'Dog'), ('cat', 'Cat')]
     Adoption_ready = [('N', 'No'), ('Y', 'Yes')]
-    name = models.CharField(max_length=100)  # Name of the Pet
-    submitter = models.CharField(max_length=100, null=True)  # Name of Submitter
+    name = models.CharField(max_length=100, verbose_name="שם")  # Name of the Pet
+    submitter = models.CharField(max_length=100, null=True, verbose_name="הוכנס על ידי")  # Name of Submitter
     species = models.CharField(max_length=30, choices=Species_choice, blank=False,
-                               default='Dog')  # What is the Pet species? (dog, cat)
-    breed = models.CharField(max_length=30, blank=True)  # Pet's breed (can leave empty)
-    description = models.TextField(blank=True)  # Description like where was found etc...
-    sex = models.CharField(max_length=30, choices=Sex_Choice, blank=True)  # Gender (can leave empty)
-    submission_date = models.DateTimeField(default=timezone.now)  # Date submitted to shelter
-    image = models.ImageField(upload_to='Animal', blank=True, default='default.png')  # for animal images
-    Adoption = models.CharField(max_length=30, choices=Adoption_ready, blank=False, default='No')
+                               default='Dog' , verbose_name="סוג")  # What is the Pet species? (dog, cat)
+    breed = models.CharField(max_length=30, blank=True, verbose_name="גזע")  # Pet's breed (can leave empty)
+    description = models.TextField(blank=True, verbose_name="תיאור")  # Description like where was found etc...
+    sex = models.CharField(max_length=30, choices=Sex_Choice, blank=True, verbose_name="מין")  # Gender (can leave empty)
+    submission_date = models.DateTimeField(default=timezone.now,verbose_name="תאריך קליטה")  # Date submitted to shelter
+    image = models.ImageField(upload_to='Animal', blank=True, default='default.png', verbose_name="העלאת תמונה")  # for animal images
+    Adoption = models.CharField(max_length=30, choices=Adoption_ready, blank=False, default='No',verbose_name="מוכן לאימוץ")
 
     def __str__(self):
         return self.name
