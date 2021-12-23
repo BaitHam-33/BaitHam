8ab49d60e6e40c68e24d34e8ba3d303a9f21c7fd
Topaz Aakal <topazaakal5@gmail.com>
2021-12-23 14:43:11 +0200

    export reports from taskboard app (excel+pdf)

--- Taskboard/urls.py
+++ Taskboard/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import all_task, task_detail, createTask
+from .views import all_task, task_detail, createTask,export_pdf,export_excel
 
 app_name = 'Taskboard'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', all_task, name='all_task'),  # View all tasks
     path('task_detail/<int:id>', task_detail, name='task_detail'),
     path('createTask/', createTask, name='createTask'),
+    path('export_pdf',export_pdf,name='export_pdf'),
+    path('export_excel',export_excel,name='export_excel'),
 ]
