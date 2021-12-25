9499c7018f6e3c9ac3f74d7f7c649ea2bed78538
Topaz Aakal <topazaakal5@gmail.com>
2021-12-25 12:35:54 +0200

    reports-Done! adding the last reports for Animal app

--- Animal/urls.py
+++ Animal/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import all_animals, add_Animal, Animal_detail, editAnimal, deleteAnimal
+from .views import all_animals, add_Animal, Animal_detail, editAnimal, deleteAnimal, export_pdf, export_excel
 
 app_name = 'Animal'
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path('animal_detail/<int:id>', Animal_detail, name='animal_detail'),
     path('editAnimal/<str:id>', editAnimal, name='editAnimal'),
     path('deleteAnimal/<str:id>', deleteAnimal, name='deleteAnimal'),
+    path('export_pdf', export_pdf, name='export_pdf'),
+    path('export_excel', export_excel, name='export_excel'),
 ]
--- Animal/views.py
+++ Animal/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render, redirect
 from Animal.forms import Add_Animal_Form
 from .models import animal, Stats
 from .filters import animalFilter
-
+from django.http import HttpResponse, FileResponse
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from datetime import datetime
+from reportlab.pdfgen import canvas
+from reportlab.lib.units import inch
+from reportlab.lib.pagesizes import letter
+import io
+import xlwt
 
 def all_animals(request):
     if request.user.is_authenticated:
@@ -91,3 +99,72 @@ def deleteAnimal(requset, id):
 
     context = {'animal': animal_obj}
     return render(requset, 'Animal/DeleteAnimal.html', context)
+
+def export_pdf(request):
+    """Function for exporting a pdf document from the system"""
+    buf = io.BytesIO()
+    c = canvas.Canvas(buf, pagesize=letter, bottomup=0)  # creat a new page
+    textob = c.beginText()  # creat a text object
+    textob.setTextOrigin(inch, inch)  # set the sizes of the text
+    pdfmetrics.registerFont(TTFont('David', 'David.ttf'))
+    textob.setFont("David", 14)  # set the font of the text
+
+    stats = Stats.objects.all()  # designate the model
+    lines = []  # creat a new list for the objects
+
+    # print all data we need
+    for stat in stats:
+        lines.append('Month number:' + str(stat.month))
+        lines.append('Entered the hoard: ' + str(stat.created))
+        lines.append('Current amount: ' + str(stat.current))
+        lines.append('Animals adopted: ' + str(stat.deleted))
+        lines.append('    ')
+        lines.append('_______________________________________')
+        lines.append('    ')
+
+    textob.textLine("Association Status Report (Changes in Quantities) for 2021")  # the title of the file
+    textob.textLine("    ")
+    for line in lines:
+        textob.textLine(line)
+
+    c.drawText(textob)
+    c.showPage()
+    c.save()  # save the file
+    buf.seek(0)
+
+    return FileResponse(buf, as_attachment=True, filename='status.pdf')
+
+
+def export_excel(request):
+    """Function for exporting an excel document from the system"""
+    response = HttpResponse(content_type='status/excel')
+    response['Content-Disposition'] = 'attachment; filename=status' + str(datetime.now()) + '.xls'
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('status')  # give a name to the sheet
+    row_num = 2  # initial row for objects
+    font_style = xlwt.XFStyle()  # set the font of the text
+    font_style.font.bold = True
+
+    ws.write(0, 0, 'Association Status Report (Changes in Quantities) for 2021', font_style)  # the title of the file
+
+    columns = ['Month number', 'Entered the hoard', 'Current amount', 'Animals adopted']  # the columns in the table
+
+    for col_num in range(len(columns)):
+        ws.write(row_num, col_num, columns[col_num], font_style)
+
+    # save all the data we need from database
+    rows = Stats.objects.all().values_list('month', 'created', 'current', 'deleted')
+
+    font_style = xlwt.XFStyle()
+
+    for row in rows:
+        row_num += 1
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, str(row[col_num]), font_style)  # enter all the data to the table
+
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+
+    wb.save(response)  # save the file
+
+    return response
