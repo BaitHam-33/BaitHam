74a7af49c20a1c475a139335cf7c2bafba5c7a14
Topaz Aakal <topazaakal5@gmail.com>
2021-12-25 11:49:24 +0200

    unit tests for volunteer app

---
+++ volunteer/tests/test_views.py
@@ -0,0 +1,17 @@
+from django.urls import reverse
+from django.test import TestCase, Client
+from volunteer.models import attendance
+from volunteer.views import *
+
+class BaseTest(TestCase):
+    def setUp(self):
+        self.login_url = reverse('loginuser')
+        self.user = {
+            'username': 'username',
+            'password': 'password' }
+        return super().setUp()
+
+class LoginTest(BaseTest):
+    def test_can_access_page(self):
+        response = self.client.get(self.login_url)
+        self.assertEqual(response.status_code, 200)
