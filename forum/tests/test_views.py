a9022b9fec7d3d3c13742d9a26bd33682c293568
avigail shekasta <avigails90@gmail.com>
2022-01-01 22:12:34 +0200

    test forum

---
+++ forum/tests/test_views.py
@@ -0,0 +1,33 @@
+from django.urls import reverse
+from django.test import TestCase, Client
+from forum.models import forum
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.obj = forum.objects.create(id=1,
+                                         name='avigail',
+                                         email='avigails90@gmail.com',
+                                         topic='test',
+                                         description='help my',)
+
+    def test_home_GET(self):
+        response = self.client.get(reverse('forum:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'adopter/Base.html')
+        self.assertTemplateUsed(response, 'forum/home.html')
+
+    def test_addInForum_POST_Valid(self):
+        animal_count = forum.objects.count()
+        response = self.client.post(reverse('forum:addInForum'), { 'name':'avigail',
+                                                                   'email':'avigails90@gmail.com',
+                                                                   'topic':'test',
+                                                                   'description':'help my',})
+
+
+        self.assertEqual(response.status_code, 302)
+
+
+
+
