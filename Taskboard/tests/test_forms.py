40481cfa463d0df92d723053724ea5e19c4b57ad
Daniel dei <daniel.hajaj121@gmail.com>
2022-01-02 14:55:14 +0200

    Check for non-data / incorrect receipt, for this form

--- Taskboard/tests/test_forms.py
+++ Taskboard/tests/test_forms.py
@@ -10,4 +10,8 @@ class TestForms(unittest.TestCase):
         'text': 'Take Rexie to get vaccinated'})
         self.assertTrue(form.is_valid())  # form is valid so true expected
 
+    def test_TaskForm_no_Data(self):
+        form = TaskForm(data={})
+        self.assertFalse(form.is_valid())  # form is not valid so false expected
+        self.assertEqual(len(form.errors), 4)  # form has 4 required fields
 
