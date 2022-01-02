75292292dc0f7bac7cf7c732f1fa095f5063d81f
Daniel dei <daniel.hajaj121@gmail.com>
2022-01-02 13:24:10 +0200

    Modifying and turning _export_excels and export_pdf tests into unit tests

--- Supplier/tests/test_urls.py
+++ Supplier/tests/test_urls.py
@@ -4,10 +4,10 @@ from Taskboard.views import export_pdf, export_excel
 
 
 class TestUrls(SimpleTestCase):
-    def test_export_pdf_url_is_resolved(self):
+    def test_export_pdf_url_resolves(self):
         url = reverse(' export_pdf')
-        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class,export_pdf)
 
-    def test_export_excel_url_is_resolved(self):
+    def test_export_excel_url_resolves(self):
         url = reverse('  export_excel')
-        print(resolve(url))
+        self.assertEquals(resolve(url).func.view_class, export_excel)
